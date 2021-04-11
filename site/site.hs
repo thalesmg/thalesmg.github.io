30731d6ee0978ad5bf7fb840f93301f79b87cfc2
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-04-11 18:36:39 -0300

    rasc

--- site/site.hs
+++ site/site.hs
@@ -1,12 +1,33 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+
+import Control.Applicative (empty)
+import Data.Maybe (fromJust)
 import           Data.Monoid (mappend)
 import           Hakyll
+import Debug.Trace
 
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
+    let postsGlob = "posts/*"
+    tags <- buildTags postsGlob (fromCapture "tags/*.html")
+
+    tagsRules tags $ \tag pat -> do
+      let title = "Posts tagged " <> tag
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll pat
+        let ctx = listField "posts" (postCtxWithTags tags) (pure posts)
+               <> constField "title" title
+               <> defaultContext
+
+        makeItem ""
+            >>= loadAndApplyTemplate "templates/archive.html" ctx
+            >>= loadAndApplyTemplate "templates/default.html" ctx
+            >>= relativizeUrls
+
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
@@ -21,20 +42,20 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    match "posts/*" $ do
+    match postsGlob $ do
         route $ setExtension "html"
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= loadAndApplyTemplate "templates/post.html"    (postCtxWithTags tags)
+            >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
             >>= relativizeUrls
 
     create ["archive.html"] $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- recentFirst =<< loadAll postsGlob
             let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
+                    listField "posts" (postCtxWithTags tags) (return posts) <>
+                    constField "title" "Archives" <>
                     defaultContext
 
             makeItem ""
@@ -46,9 +67,9 @@ main = hakyll $ do
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- recentFirst =<< loadAll postsGlob
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
+                    listField "posts" (postCtxWithTags tags) (return posts) <>
                     defaultContext
 
             getResourceBody
@@ -60,7 +81,38 @@ main = hakyll $ do
 
 
 --------------------------------------------------------------------------------
+postCtxWithTags :: Tags -> Context String
+postCtxWithTags tags =
+     tagsField "tags" tags
+  -- <> listField "tagsList" (field "tag" $ pure . itemBody) produceTags
+  <> allTagsField "tagsList" tags
+  <> postCtx
+
+produceTags :: Compiler [Item String]
+produceTags = do
+  i <- getResourceString
+  tgs <- getTags (itemIdentifier i)
+  traverse makeItem tgs
+
+-- https://github.com/ysndr/blog/blob/5e10a521ca3faea94f0d9c536f079d4e5a5780db/generator/Fields.hs#L205
+allTagsField :: String -> Tags -> Context String
+allTagsField name tags = listFieldWith name tagCtx mkPostTags
+  where
+    tagCtx :: Context String
+    tagCtx = field "name" (pure . itemBody)
+          <> field "url" mkTagUrl
+
+    mkTagUrl :: Item String -> Compiler String
+    mkTagUrl tagItem = toUrl <$> (fmap fromJust . getRoute . tagsMakeId tags . itemBody $ tagItem)
+
+    mkPostTags :: Item String -> Compiler [Item String]
+    mkPostTags postItem = do
+      tgs <- getTags (itemIdentifier postItem)
+      if null tgs
+        then empty
+        else mapM makeItem tgs
+
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
+    dateField "date" "%B %e, %Y" <>
     defaultContext
