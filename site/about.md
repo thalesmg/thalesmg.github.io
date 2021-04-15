b86085bc0e3116c8f9618d92321da38dcfdad849
Thales Macedo Garitezi <thalesmg@gmail.com>
2021-04-15 20:40:03 -0300

    Tags (#2)

---
+++ site/about.md
@@ -0,0 +1,12 @@
+---
+title: About
+---
+
+I'm a software developer currently based in Brazil and with experience
+in web backends. Occasionally dabbles with the frontend and
+infrastructure. The kinds of programming languages I like the most are
+strongly typed ones, like Haskell, OCaml, Rust and similar friends. I
+also enjoy learning new stuff that help me think in different ways.
+
+- github: [https://github.com/thalesmg](https://github.com/thalesmg)
+- gitlab: [https://gitlab.com/thalesmg](https://gitlab.com/thalesmg)
--- site/site.hs
+++ site/site.hs
@@ -1,12 +1,31 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
+
+import           Control.Applicative (empty)
+import           Data.Maybe          (fromJust)
 import           Hakyll
 
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
+    let postsGlob = "posts/*"
+    tags <- buildTags postsGlob (fromCapture "tags/*.html")
+
+    tagsRules tags $ \tag pat -> do
+      let title = "Posts tagged " <> tag
+      route idRoute
+      compile $ do
+        posts <- recentFirst =<< loadAll pat
+        let ctx = listField "posts" (postCtxWithTags tags) (pure posts)
+               <> constField "title" title
+               <> defaultContext
+
+        makeItem ""
+            >>= loadAndApplyTemplate "templates/post-list.html" ctx
+            >>= loadAndApplyTemplate "templates/default.html" ctx
+            >>= relativizeUrls
+
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
@@ -15,26 +34,26 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    match (fromList ["about.rst", "contact.markdown"]) $ do
+    match (fromList ["about.md"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    match "posts/*" $ do
+    match postsGlob $ do
         route $ setExtension "html"
         compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= loadAndApplyTemplate "templates/post.html"    (postCtxWithTags tags)
+            >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
             >>= relativizeUrls
 
     create ["archive.html"] $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- recentFirst =<< loadAll postsGlob
             let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
+                    listField "posts" (postCtxWithTags tags) (return posts) <>
+                    constField "title" "Archives" <>
                     defaultContext
 
             makeItem ""
@@ -46,9 +65,9 @@ main = hakyll $ do
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- recentFirst =<< loadAll postsGlob
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
+                    listField "posts" (postCtxWithTags tags) (return posts) <>
                     defaultContext
 
             getResourceBody
@@ -60,7 +79,33 @@ main = hakyll $ do
 
 
 --------------------------------------------------------------------------------
+postCtxWithTags :: Tags -> Context String
+postCtxWithTags tags =
+     tagsField "tags" tags
+  <> allTagsField "tagsList" tags
+  <> postCtx
+
+-- https://github.com/ysndr/blog/blob/5e10a521ca3faea94f0d9c536f079d4e5a5780db/generator/Fields.hs#L205
+allTagsField :: String -> Tags -> Context String
+allTagsField name tags = listFieldWith name tagCtx mkPostTags
+  where
+    tagCtx :: Context String
+    tagCtx = field "name" (pure . itemBody)
+          <> field "url" mkTagUrl
+
+    mkTagUrl :: Item String -> Compiler String
+    mkTagUrl tagItem = do
+      mRoute <- getRoute . tagsMakeId tags . itemBody $ tagItem
+      pure . toUrl . fromJust $ mRoute
+
+    mkPostTags :: Item String -> Compiler [Item String]
+    mkPostTags postItem = do
+      tgs <- getTags (itemIdentifier postItem)
+      if null tgs
+        then empty
+        else mapM makeItem tgs
+
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
-    defaultContext
+     dateField "date" "%Y-%m-%d"
+  <> defaultContext
