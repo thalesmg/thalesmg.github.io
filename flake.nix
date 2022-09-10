a62aa20a4234f1a41606870916718da822635803
Thales Macedo Garitezi <thalesmg@gmail.com>
2022-09-10 19:06:38 -0300

    parece que nix é imbatível aqui

--- flake.nix
+++ flake.nix
@@ -14,63 +14,73 @@
       pkgs = import nixpkgs {
         system = "x86_64-linux";
       };
-      hpkgs = pkgs.haskellPackages.override {
-        overrides = hSelf: hSuper: {
-          # pandoc =
-          #   pkgs.lib.pipe
-          #     (hSelf.callHackage "pandoc" "2.11.4" {})
-          #     [
-          #       doJailbreak
-          #       dontCheck
-          #     ];
-          tmg-site-builder = hSelf.callCabal2nix "site" ./site {};
-          # hakyll = pkgs.lib.pipe
-          #   hSuper.hakyll
-          #   [
-          #     unmarkBroken
-          #     doJailbreak
-          #   ];
-          # hls-fourmolu-plugin = dontCheck hSuper.hls-fourmolu-plugin;
-        };
-      };
-      doJailbreak = pkgs.haskell.lib.doJailbreak;
-      unmarkBroken = pkgs.haskell.lib.unmarkBroken;
-      dontCheck = pkgs.haskell.lib.dontCheck;
-      site-builder = hpkgs.tmg-site-builder;
-      site-dir = ./site;
-      site = pkgs.runCommand "tmg-site" {
-        LANG = "en_US.UTF-8";
-        LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
-        buildInputs = [
-          pkgs.coqPackages.coq
-          pkgs.coqPackages.serapi
-          pkgs.python3Packages.alectryon
-        ];
-      } ''
-        mkdir -p dist
-        mkdir -p $out
-        cp -R ${site-dir}/* dist/
-        cd dist
-        ${site-builder}/bin/site clean
-        ${site-builder}/bin/site build
-        cp -r _site/* $out/
-      '';
+      # hpkgs = pkgs.haskellPackages.override {
+      #   overrides = hSelf: hSuper: {
+      #     # pandoc =
+      #     #   pkgs.lib.pipe
+      #     #     (hSelf.callHackage "pandoc" "2.11.4" {})
+      #     #     [
+      #     #       doJailbreak
+      #     #       dontCheck
+      #     #     ];
+      #     tmg-site-builder = hSelf.callCabal2nix "site" ./site {};
+      #     # hakyll = pkgs.lib.pipe
+      #     #   hSuper.hakyll
+      #     #   [
+      #     #     unmarkBroken
+      #     #     doJailbreak
+      #     #   ];
+      #     # hls-fourmolu-plugin = dontCheck hSuper.hls-fourmolu-plugin;
+      #   };
+      # };
+      # doJailbreak = pkgs.haskell.lib.doJailbreak;
+      # unmarkBroken = pkgs.haskell.lib.unmarkBroken;
+      # dontCheck = pkgs.haskell.lib.dontCheck;
+      # site-builder = hpkgs.tmg-site-builder;
+      # site-dir = ./site;
+      # site = pkgs.runCommand "tmg-site" {
+      #   LANG = "en_US.UTF-8";
+      #   LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+      #   buildInputs = [
+      #     pkgs.coqPackages.coq
+      #     pkgs.coqPackages.serapi
+      #     pkgs.python3Packages.alectryon
+      #   ];
+      # } ''
+      #   mkdir -p dist
+      #   mkdir -p $out
+      #   cp -R ${site-dir}/* dist/
+      #   cd dist
+      #   ${site-builder}/bin/site clean
+      #   ${site-builder}/bin/site build
+      #   cp -r _site/* $out/
+      # '';
     in
       {
-        defaultPackage.x86_64-linux = site;
-        devShell.x86_64-linux = hpkgs.shellFor {
-          packages = p: [
-            p.tmg-site-builder
-          ];
+        devShell.x86_64-linux = pkgs.mkShell {
           buildInputs = [
-            pkgs.cabal-install
-            pkgs.haskell-language-server
-            hpkgs.hakyll
-            hpkgs.hakyll-alectryon
             pkgs.coqPackages.coq
             pkgs.coqPackages.serapi
             pkgs.python3Packages.alectryon
           ];
         };
+        # defaultPackage.x86_64-linux = site;
+      #   devShell.x86_64-linux = hpkgs.shellFor {
+      #     packages = p: [
+      #       # p.tmg-site-builder
+      #       # p.coqPackages.coq
+      #       # p.coqPackages.serapi
+      #       # p.python3Packages.alectryon
+      #     ];
+      #     buildInputs = [
+      #       # pkgs.cabal-install
+      #       # pkgs.haskell-language-server
+      #       # hpkgs.hakyll
+      #       # hpkgs.hakyll-alectryon
+      #       pkgs.coqPackages.coq
+      #       pkgs.coqPackages.serapi
+      #       pkgs.python3Packages.alectryon
+      #     ];
+      #   };
       };
 }
